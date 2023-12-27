a5a34c3e47d4e52e43405fb7851bd7d4c2eb45fb
Jappie Klooster <jappieklooster@hotmail.com>
2023-12-27 13:44:30 +0100

    whatever flake

--- flake.nix
+++ flake.nix
@@ -4,20 +4,17 @@
   outputs = { self, nixpkgs }:
     let
 
-    pkgs = import nixpkgs.legacyPackages.x86_64-linux {
+    pkgs = import nixpkgs {
   config = {
+    currentSystem = "";
     # set an agda https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/agda.section.md#agda-agda
     packageOverrides = pkgs: {
       agsy = pkgs.agda.withPackages (p: [ p.standard-library ]);
     };
   };
 
-}
-    in
-    {
-
-    packages.x86_64-linux.default =
-
+};
+agdaMode =
 pkgs.runCommand "agda-mode" {
   agsy=pkgs.agsy;
 } ''
@@ -32,5 +29,12 @@ cp -R $MODE/* $out/share/emacs/site-lisp/elpa/agda-mode-0.0.1/
 
 
 '';
+    in
+    {
+
+    packages.x86_64-linux.default = agdaMode;
+    packages.x86_64-linux.agdaMode = agdaMode;
+    packages.x86_64-linux.agsy = pkgs.agsy;
+
   };
 }
